0fac7fae0d9cd31303c67b1706b6ebc97171c111
Imbirsh0 <litdanila@gmail.com>
2025-09-16 23:50:48 +0300

    Add files via upload

---
+++ Praktsharp16/Program.cs
@@ -0,0 +1,7 @@
+﻿int t = int.Parse(Console.ReadLine());
+int v = int.Parse(Console.ReadLine());
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"a) 5 10     б) 100 {t}      в) {x} 25");
+Console.WriteLine($"   7 10        1949 {v}        {x} {y}");
