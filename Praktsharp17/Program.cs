bf012c3c65c7bd605419f2702c2849713ea3532c
Imbirsh0 <litdanila@gmail.com>
2025-09-16 23:53:09 +0300

    Add files via upload

---
+++ Praktsharp17/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"a) 2 кг     б) {a} 1      в) {x} {y}");
+Console.WriteLine($"   13 17       19 {b}        5 {y}");
+
