db5599ad239d7cb8e53dae1ef10061953b80f5c8
Imbirsh0 <litdanila@gmail.com>
2025-09-16 23:35:35 +0300

    Add files via upload

---
+++ Praktsharp13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int pr = a - 1;
+int nx = a + 1;
+
+Console.WriteLine($"Предыдущее: {pr}");
+Console.WriteLine($"Следующее: {nx}");
