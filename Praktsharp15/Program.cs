ce504664b7546407135da85205e9892e18f51ec4
Imbirsh0 <litdanila@gmail.com>
2025-09-16 23:43:15 +0300

    Add files via upload

---
+++ Praktsharp15/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{a} {b} {c} {d}");
